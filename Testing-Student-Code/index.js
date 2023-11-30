971cce38ef6191f4eb4776c5c592c2169c520714
John <128085030+IzzyCraze365@users.noreply.github.com>
2023-11-29 21:47:29 -0500

    End of class 11-29, Wednesday

---
+++ Testing-Student-Code/index.js
@@ -0,0 +1,9 @@
+console.log("titanic".toUpperCase());
+console.log("QUIETLY".toLowerCase);
+console.log("Java".repeat(10));
+console.log("berry".charAt(1));
+console.log("berry".charAt(0));
+console.log("banana".includes("nan"));
+console.log("banana".endsWith("ana"));
+console.log("blueberry".replace("blue", "black"));
+
