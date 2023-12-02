d5de4e3fc4f9455f7697bfb04fa20cd5482d72b1
John <128085030+IzzyCraze365@users.noreply.github.com>
2023-12-01 20:17:41 -0500

    Office Hours aid -Test Student Code

--- Testing-Student-Code/index.js
+++ Testing-Student-Code/index.js
@@ -8,14 +8,37 @@ console.log("banana".endsWith("ana"));
 console.log("blueberry".replace("blue", "black"));
 
 // With Booleans //
-let myBool = ("pie" + true);
+let myBool = "pie" + true;
 console.log(myBool);
 
-myBool = ("pie" + false);
+myBool = "pie" + false;
 console.log(myBool);
 
 let myBool2 = false + 8;
-console.log(myBool2)
+console.log(myBool2);
 
 myBool2 = true + 8;
-console.log(myBool2)
+console.log(myBool2);
+
+function greeter(name) {
+  console.log(name);
+  if (name === "darth vader") {
+    console.log("Go away!");
+  } else if (name === "luke skywalker") {
+    console.log("Hello luke skywalker, you are a wonderful friend!");
+  } else if (name === "lex luthor") {
+    console.log("Be gone, you villain!!!");
+  } else {
+    console.log(`welcome ${name}`);
+  }
+
+  if(name.includes("darth")){
+    console.log(`I recognize you ${name}`);
+  }
+}
+
+greeter("john");
+greeter("darth vader");
+greeter("luke escoto");
+greeter("luke skywalker");
+greeter("lex luthor");
