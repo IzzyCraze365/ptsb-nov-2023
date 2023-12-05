70e1173829c27a5db1fc340364b5040c396cc336
John <128085030+IzzyCraze365@users.noreply.github.com>
2023-12-04 20:56:50 -0500

    Monday - Dec. 4, 2023 Class completed parameters_arguments

--- 2-js/functions/Old_MacDonald.js
+++ 2-js/functions/Old_MacDonald.js
@@ -21,23 +21,22 @@ function singOldMacDonald(animal, sound) {
   singOldMacDonald("trebuchet", "swing");
   
 // Arrow Function
- function singOldMacDonald(animal, sound) { 
-    return () => console.log(`Old MacDonald had a farm,
-      Ee i ee i o!
-  And on his farm he had some ${animal}s,
-      Ee i ee i oh!
-  With a ${sound}-${sound} here,
-      And a ${sound}-${sound} there.
-  Here a ${sound}, there a ${sound},
-      Everywhere a ${sound}-${sound}.
-  Old MacDonald had a farm,
-      Ee i ee i o!!!
-  `);
-  }
+ let singOldMacDonald2 = (animal, sound) => { 
+ console.log(`Old MacDonald had a farm,
+    Ee i ee i o!
+And on his farm he had some ${animal}s,
+    Ee i ee i oh!
+With a ${sound}-${sound} here,
+    And a ${sound}-${sound} there.
+Here a ${sound}, there a ${sound},
+    Everywhere a ${sound}-${sound}.
+Old MacDonald had a farm,
+    Ee i ee i o!!!
+  `)}
 
-  singOldMacDonald("pig", "oink");
-  singOldMacDonald("cow", "moo");
-  singOldMacDonald("horse", "neigh");
-  singOldMacDonald("cat", "meow");
-  singOldMacDonald("dog", "woof");
-  singOldMacDonald("trebuchet", "swing");
+  singOldMacDonald2("pig", "oink");
+  singOldMacDonald2("cow", "moo");
+  singOldMacDonald2("horse", "neigh");
+  singOldMacDonald2("cat", "meow");
+  singOldMacDonald2("dog", "woof");
+  singOldMacDonald2("trebuchet", "swing");
