441b399d4d8a62a901b1a538e4693577c22d73cf
John <128085030+IzzyCraze365@users.noreply.github.com>
2023-12-07 18:45:57 -0500

    Thursday - Dec. 7, 2023 Start of Class

--- Testing-Student-Code/index.js
+++ Testing-Student-Code/index.js
@@ -1,44 +1,26 @@
-console.log("titanic".toUpperCase());
-console.log("QUIETLY".toLowerCase);
-console.log("Java".repeat(10));
-console.log("berry".charAt(1));
-console.log("berry".charAt(0));
-console.log("banana".includes("nan"));
-console.log("banana".endsWith("ana"));
-console.log("blueberry".replace("blue", "black"));
-
-// With Booleans //
-let myBool = "pie" + true;
-console.log(myBool);
-
-myBool = "pie" + false;
-console.log(myBool);
-
-let myBool2 = false + 8;
-console.log(myBool2);
-
-myBool2 = true + 8;
-console.log(myBool2);
-
-function greeter(name) {
-  console.log(name);
-  if (name === "darth vader") {
-    console.log("Go away!");
-  } else if (name === "luke skywalker") {
-    console.log("Hello luke skywalker, you are a wonderful friend!");
-  } else if (name === "lex luthor") {
-    console.log("Be gone, you villain!!!");
+function computeClosestToZero(ts) {
+  // Write your code here
+  let closeTemp = 10;
+  if (ts.length === 0){
+      closeTemp = 0;
   } else {
-    console.log(`welcome ${name}`);
+      for(let i=0; i <= ts.length;i++){
+          if(Math.abs(ts[i]) < Math.abs(closeTemp)){
+              closeTemp = ts[i]
+          }else if (ts[i] === closeTemp){
+              closeTemp = ts[i];
+          }else if (Math.abs(ts[i]) === Math.abs(closeTemp)){
+          closeTemp = Math.abs(ts[i]);
+      }
   }
 
-  if(name.includes("darth")){
-    console.log(`I recognize you ${name}`);
-  }
 }
 
-greeter("john");
-greeter("darth vader");
-greeter("luke escoto");
-greeter("luke skywalker");
-greeter("lex luthor");
+  // To debug: console.error('Debug messages...');
+  
+  return closeTemp;
+}
+
+let temps = [7, -10, 13, 8, 4, -7, -12, -3, 3, -9, 6, -1, -6, 7]; //Array of temperatures
+
+console.log(`The temperature closest to zero is ${computeClosestToZero(temps)}`);
