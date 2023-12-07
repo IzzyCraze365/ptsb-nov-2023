3e33f94702dc0029df86488c4232f178d2b34078
John <128085030+IzzyCraze365@users.noreply.github.com>
2023-12-06 19:53:19 -0500

    Wednesday - Dec. 6, 2023 scope & hoisting lesson

--- 2-js/functions/arrow_functions.js
+++ 2-js/functions/arrow_functions.js
@@ -20,48 +20,50 @@
 // ? Concise body
 
 let hi = () => {
-    console.log("something");
-    //   additional code here in block body function {}
-  };
-  
-  hi();
-  
-  let hi2 = () => console.log("something2");
-  
-  hi2();
-  
-  let hello = () => {
-    let greeting = "hello";
-    let moreInfo = "steve";
-    return `${greeting}, ${moreInfo}`;
-  };
-  
-  console.log(hello());
-  
-  // ? Implicit Return
-  
-  let apples1 = (x) => `There are ${x} apples`;
-  
-  console.log(apples1(5));
-  
-  // ? Explicit Return
-  let apples2 = (y) => {
-    // additional code here...
-    return `There are ${y} apples`;
-  };
-  
-  console.log(apples2(10));
-  
-  let myFName = (fName) => console.log(fName);
-  
-  myFName("Bob");
-  
-  const tipCalc = (subTotal) => {
-    let tip = subTotal * 0.2;
-    let total = subTotal + tip;
-    return total.toFixed(2);
-  };
-  
-  console.log(tipCalc(10));
-  console.log(tipCalc(20));
-  console.log(tipCalc(15));
+  console.log("something");
+  //   additional code here in block body function {}
+};
+
+hi();
+
+let hi2 = () => console.log("something2");
+
+hi2();
+
+let hello = () => {
+  let greeting = "hello";
+  let moreInfo = "steve";
+  return `${greeting}, ${moreInfo}`;
+};
+
+console.log(hello());
+
+// ? Implicit Return
+
+let apples1 = (x) => `There are ${x} apples`;
+
+console.log(apples1(5));
+
+// ? Explicit Return
+let apples2 = (y) => {
+  // additional code here...
+  return `There are ${y} apples`;
+};
+
+console.log(apples2(10));
+
+let myFName = (fName) => console.log(fName);
+
+myFName("Bob");
+
+const tipCalc = (subTotal) => {
+  let tip = subTotal * 0.2;
+  let total = subTotal + tip;
+  return total.toFixed(2);
+};
+
+console.log(tipCalc(10));
+console.log(tipCalc(20));
+console.log(tipCalc(15));
+
+console.log(```hello there```);
