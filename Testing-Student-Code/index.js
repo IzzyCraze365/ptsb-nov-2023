506ffacad8272a34c82f92a380f56e7d58b5ab9a
John <128085030+IzzyCraze365@users.noreply.github.com>
2023-12-08 20:51:25 -0500

    office hours

---
+++ Testing-Student-Code/index.js
@@ -0,0 +1,29 @@
+
+
+
+
+let currentLight = "Red";
+trafficLight(currentLight);
+
+
+function trafficLight(light) {
+    if (light === "Red") {
+      console.log("STOP");
+    } else if (light === "Green") {
+      console.log("GO!!!!");
+    } else if (light === "Yellow") {
+      console.log("Slow Down");
+    } else {
+      console.log(`${light} is not a valid option`);
+    }
+  }
+
+
+  function snackTime(food) {
+      food = `Bring me home some ${food}`;
+      return food;
+    }
+
+
+let treat = "Potatoes"
+console.log(snackTime(treat));
