2f2e82d8878ed9620bbe448e405e12aa18f5cb30
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-01-04 17:44:29 -0500

    Thursday, January 4, 2024 - Before Class

--- projects/Replits/mission-countdown-timer/index.js
+++ projects/Replits/mission-countdown-timer/index.js
@@ -13,9 +13,30 @@ function startCountdown() {
 }
 
 function startIntervalCount() {
+  let countDown = 5;
+  let time = setInterval(() => {
+    if (countDown > 0) {
+      intervalDisplay.textContent = countDown;
+      console.log(countDown, "20"); //! TEST
+      countDown--;
+    } else if (countDown === 0) {
+      intervalDisplay.textContent = "BLASTOFF!!!";
+      console.log(countDown, "Blastoff"); //! TEST
+      countDown--;
+    } else {
+      clearInterval(time);
+      console.log("Pausing"); //! TEST
+    }
+  }, 1000);
+  console.log(countDown, "22"); //! TEST
+}
+
+// TODO I did not use anything below this line
+
+/* function startIntervalCount() {
   console.log("Start Countdown Button Clicked");
   let count = 5;
-  setInterval(countdown, 1000, count);
+  setInterval(()=>{countdown, 1000, count};
   console.log("Test");
   /*  if (countDown === 0) {
       intervalDisplay.textContent = "BLASTOFF!!!";
@@ -24,21 +45,21 @@ function startIntervalCount() {
       intervalDisplay.textContent = countDown;
   } else {
     intervalDisplay.textContent = "Launch Failed";
-  } */
+  } 
 }
-
+ */
 function countdown(number) {
   console.log("COUNTDOWN FUNC", number);
   if (number > 0) {
     intervalDisplay.textContent = number;
     number--;
-    console.log("Number",number)
+    console.log("Number", number);
   } else {
     intervalDisplay.textContent = "BLASTOFF!!!";
   }
   count = number;
-  console.log("Number",number)
-  console.log("Count",count)
+  console.log("Number", number);
+  console.log("Count", count);
   return count;
   /*     if (number === 0) {
         intervalDisplay.textContent = "BLASTOFF!!!";
