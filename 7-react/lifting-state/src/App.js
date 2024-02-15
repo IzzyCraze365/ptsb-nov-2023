5a8679d3a0f39cc8bd5d7a8b4ee749bb6e25bb31
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-02-14 19:35:52 -0500

    Wednesday, February 14, 2024 - Middle of Class - React

--- 7-react/lifting-state/src/App.js
+++ 7-react/lifting-state/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React, { useState } from "react";
+
+import Form from "./components/Form";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
 
 function App() {
+  // STATE
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [userName, setUserName] = useState("Amit");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      {!loggedIn ? (
+        <Form userName={userName} setUserName={setUserName} />
+      ) : (
+        <Home />
+      )}
     </div>
   );
 }
