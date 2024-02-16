1059c3462fd6f2a03c1bb1d56f336c25cff1340c
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-02-15 21:57:18 -0500

    Thursday, February 15, 2024 - End of Class - React-Useeffect

--- 7-react/useeffect-demo/src/App.js
+++ 7-react/useeffect-demo/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  //const [firstItem, functionToUpdateFirstItem] = useState()  //! This is the Example
+  const [show, setShow] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        {show ? "hide" : "show"}
+      </button>{" "}
+      {/* This is a ternary, {variable ? true response ? false response}  */}
+      {show && <h1>Revealed compoenent</h1>}
     </div>
   );
 }
