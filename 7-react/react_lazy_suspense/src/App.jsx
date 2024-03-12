230212f22450ffa5b2eb05c885b944c3890e4161
John <128085030+IzzyCraze365@users.noreply.github.com>
2024-03-11 20:32:04 -0400

    Monday. March 11, 2024 - Class Challenge

--- 7-react/react_lazy_suspense/src/App.jsx
+++ 7-react/react_lazy_suspense/src/App.jsx
@@ -1,35 +1,75 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+
+import { Auth, About, Home } from "./components";
+import { Link, Routes, Route } from "react-router-dom";
+import { Suspense } from "react";
+
+const NavWrapper = ({ children }) => {
+  return (
+    <div className="border-2 space-x-4 rounded-lg p-4 min-w-96 max-w-96 centered mx-auto mb-8">
+      {children}
+    </div>
+  );
+};
+
+const MySpinner = ({ text, color }) => {
+  return (
+    <div>
+      <h2>{text}</h2>
+      <svg className="animate-spin h-5 w-5 mx-auto my-4">
+        <path
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          fill={color}
+        ></path>
+      </svg>
+    </div>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavWrapper>
+        <Link to="/Home">Home</Link>
+        <Link to="/About">About</Link>
+        <Link to="/Auth">Auth</Link>
+      </NavWrapper>
+
+      <Routes>
+        <Route path="/" element={<div>Welcome to my page</div>} />
+        <Route
+          path="/Home"
+          element={
+            <Suspense
+              fallback={<MySpinner text="Loading Home..." color="white" />}
+            >
+              <Home />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/Auth"
+          element={
+            <Suspense
+              fallback={<MySpinner text="Loading Auth..." color="white" />}
+            >
+              <Auth />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/About"
+          element={
+            <Suspense
+              fallback={<MySpinner text="Loading About..." color="white" />}
+            >
+              <About />
+            </Suspense>
+          }
+        />
+      </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
